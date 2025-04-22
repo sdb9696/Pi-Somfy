8ed44b7c63dae511b7ba9bcca4fe9fde362140f8
Steven B <51370195+sdb9696@users.noreply.github.com>
2025-04-22 18:11:05 +0100

    Add shadcn-uiimports to top of app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,14 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+import { Badge } from "@/components/ui/badge";
+import { Settings, CalendarClock, Blinds, ArrowUpDown } from 'lucide-react';
 import { ThemeProvider } from "@/components/theme-provider";
 import ThemeToggle from "@/components/ThemeToggle";
 import { getConfig } from './services/api';
